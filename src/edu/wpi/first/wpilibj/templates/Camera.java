715e8624986ff554987dd618cfcb4122b03a4677
team348 <klevin.gluo@gmail.com>
2012-02-01 22:44:53 -0500

    3rd commit

---
+++ src/edu/wpi/first/wpilibj/templates/Camera.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.camera.AxisCamera;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author team348
+ */
+public class Camera extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    AxisCamera camera;
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void findBall() {
+        
+    }
+    
+    public void trackBall() {
+        
+    }
+}
---
+++ src/edu/wpi/first/wpilibj/templates/Compressor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.wpi.first.wpilibj.templates;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ * @author team348
+ */
+public class Compressor extends Subsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
