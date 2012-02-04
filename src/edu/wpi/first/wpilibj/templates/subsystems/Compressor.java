ab9925f1cf32f1ea9244c76e497b3f57d45ba1fe
team348 <klevin.gluo@gmail.com>
2012-02-03 20:44:12 -0500

    3rd commit

--- src/edu/wpi/first/wpilibj/templates/subsystems/Compressor.java
+++ src/edu/wpi/first/wpilibj/templates/subsystems/Compressor.java
@@ -4,6 +4,8 @@
  */
 package edu.wpi.first.wpilibj.templates.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -13,9 +15,24 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Compressor extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+    protected Relay compressor;
+    protected DigitalInput pressureSwitch;
+    private boolean on;
+    
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void start() {
+        compressor.set(Relay.Value.kOn);
+    }
+    
+    public void stop() {
+        compressor.set(Relay.Value.kOff);
+    }
+    
+    public boolean isFull() {
+        return !pressureSwitch.get();
     }
 }
