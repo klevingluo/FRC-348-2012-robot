23a04064b3536ef282b859b598d1939065f3b3c4
team348 <klevin.gluo@gmail.com>
2012-02-11 19:46:18 -0500

    added and implemented ps2 controller class, more misc code

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -8,12 +8,10 @@
 package edu.wpi.first.wpilibj.templates;
 
 
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.templates.commands.CommandBase;
-import edu.wpi.first.wpilibj.templates.commands.Shoot;
-import edu.wpi.first.wpilibj.templates.commands.LeftDrive;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.Joystick;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,46 +21,140 @@ import edu.wpi.first.wpilibj.templates.commands.LeftDrive;
  * directory.
  */
 public class RobotTemplate extends IterativeRobot {
-
-    Command autonomousCommand;
-    Command driveCommand;
-
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    
+    /*
+     * the value used to equalize the drives if they are moving in the same direction
+     * must be between 0 and 1
+     * lower values will make the robot veer more easily
+     */
+    public static double EQUALIZATION_FACTOR = 0.2;
+    
+    /*
+     * if the difference b/w the motor values after the equalization divisor is applied, they are set to be equal.
+     * must be between 0 and 2
+     * higher values will make the robot drive straighter
+     */
+    public static double EQUALIZATION_THRESHOLD = 0.1;
+    
+    /*
+     * slows down acceleration if input changes faster than this
+     * must be between 0 and 2
+     * higher values will make the robot limit acceleration more
+     */
+    public static double ACCELERATION_THRESHOLD = 0.03;
+    
+    /*
+     * slows down acceleration by this factor if input acceleration exceeds the limit above
+     * must be between 0 and 1
+     * lower values will make the robot acclerate slower when limiting is in effect.
+     */
+    public static double ACCELERATION_FACTOR = 0.7;
+    
+    public static double FP_ACTIVATION_THRESHOLD = 0.7;
+    
+    private Jaguar rightDrive;
+    private Jaguar leftDrive;
+    private Jaguar rightDriveFP;
+    private Jaguar leftDriveFP;
+    private Joystick driverLeft;
+    private Joystick driverRight;
+    
+    /*
+     * the values curently being sent to the jaguars
+     */
+    private double lastLeftInput = 0;
+    private double lastRightInput = 0;
+    
     public void robotInit() {
-        // instantiate the command used for the autonomous period
-        //autonomousCommand = new ExampleCommand();
+        rightDrive = new Jaguar(5);
+        rightDriveFP = new Jaguar(6);
+        
+        leftDrive = new Jaguar(7);
+        leftDriveFP = new Jaguar(8);
+        
+        driverLeft = new Joystick(2);
+        driverRight = new Joystick(1);
 
-        // Initialize all subsystems
-        CommandBase.init();
-    }
-
-    public void autonomousInit() {
-        // schedule the autonomous command (example)
-        autonomousCommand.start();
     }
 
     /**
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-    }
 
-    public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to 
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		autonomousCommand.cancel();
     }
 
     /**
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-
+        drive(driverLeft.getY(), driverRight.getY());
+        System.out.println(Math.abs(driverLeft.getY() - lastLeftInput));
     }
+    
+    private void drive(double leftJoystick, double rightJoystick) {
+        
+        double leftPower = leftJoystick;
+        double rightPower = rightJoystick;
+        
+        /*
+         * slows down acceleration if joystick input changes too quickly
+         */
+        if (leftPower > 0 == rightPower > 0) {
+            if(Math.abs(leftPower - lastLeftInput) > ACCELERATION_THRESHOLD)
+                if(leftPower < -0.05)
+                    leftPower = lastLeftInput - ACCELERATION_THRESHOLD;
+                else if (leftPower > 0.05)
+                    leftPower = lastLeftInput + ACCELERATION_THRESHOLD;
+                else
+                    leftPower = 0;
+            if(Math.abs(rightPower - lastRightInput) > ACCELERATION_THRESHOLD)
+                if (rightPower < -0.05)
+                    rightPower = lastRightInput - ACCELERATION_THRESHOLD;
+                else if (rightPower > 0.05)
+                    rightPower = lastRightInput + ACCELERATION_THRESHOLD;
+                else
+                    rightPower = 0;
+        }
+        
+         /*
+         * if the difference of the 2 power values are within the threshold, sets both to their average
+         */
+        /*if (Math.abs(leftPower - rightPower) < EQUALIZATION_THRESHOLD) {
+            double averagePower = (leftPower + rightPower)/2;
+            leftPower = averagePower;
+            rightPower = averagePower;
+        }*/
+        
+        /*
+         * if both drives are moting in the same direction, sets them closer to each other.
+         */
+        /*if(leftJoystick < 0 == rightJoystick < 0) {
+            leftPower -= (leftJoystick - rightJoystick)*EQUALIZATION_FACTOR;
+            rightPower -= (rightJoystick - leftJoystick)*EQUALIZATION_FACTOR;
+        }*/
+        
+        lastLeftInput = leftPower;
+        lastRightInput = rightPower;
+        
+        if (Math.abs(leftPower) > FP_ACTIVATION_THRESHOLD) {
+            leftDriveFP.set(leftPower);
+        } else {
+            leftDriveFP.set(0);
+        }
+        if (Math.abs(rightPower) > FP_ACTIVATION_THRESHOLD) {
+            rightDriveFP.set(rightPower);
+        } else {
+            rightDriveFP.set(0);
+        }
+        
+        rightDrive.set(rightPower);
+        leftDrive.set(leftPower);
+        
+    }
+    
 }
